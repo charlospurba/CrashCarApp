1a78d84c008b39ccca36729e9c63eabc3fd1c8d4
Charlos Purba <charlospurba048@gmail.com>
2024-06-27 15:24:22 +0700

    Initial Commit

---
+++ CarshCar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarshCar
+//
+//  Created by Foundation-005 on 27/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
